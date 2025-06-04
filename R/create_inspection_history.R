ab72064237c416a71e66e84b0615b61654a9f0b8
Sam Henderson-Palmer <samhendersonpalmer@gmail.com>
2025-06-04 15:37:44 +0100

    Specified timezone for clarity but doesn't appear to have made a difference to saved dataset

--- R/create_inspection_history.R
+++ R/create_inspection_history.R
@@ -88,7 +88,7 @@ combined_datastores_grading <- combined_datastores_variables |>
          (starts_with("Quality_") & !ends_with("_change")),
          URL) |> 
   # We can add multiple date formats to check for
-  mutate(across(c("Publication_of_Latest_Grading", "Last_inspection_Date"), ~parse_date_time(.x, c("dmy")))) |> 
+  mutate(across(c("Publication_of_Latest_Grading", "Last_inspection_Date"), ~parse_date_time(.x, c("dmy"), tz = "GMT"))) |> 
   # convert datetimes to just dates
   mutate(across(c("Publication_of_Latest_Grading", "Last_inspection_Date"), as.Date))
   
